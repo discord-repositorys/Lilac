5dd85e567f126ff0bac0113f03e7e22f12f06d28
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-24 14:46:20 -0400

    move embed

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const mongoose = require('mongoose');
 require("./modules/functions.js")(client);
 require("./embed.js")(client);
 
+
+
 function clean(text) {
   if (typeof(text) === "string")
     return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
