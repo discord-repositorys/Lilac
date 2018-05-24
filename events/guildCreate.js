346e007581d0dacbc9ae5be1cbf0397ff28bd537
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-24 14:55:19 -0400

    remove comments

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,6 +3,5 @@ module.exports = (client, guild) => {
    G.create({ _id: guild.id, prefix: "d." }, (err, g) => {
   if(err) client.logger.error(err);
   else client.logger.log(`Added config for ${guild.name} (${guild.id})`);
-  client.channels.get("447177859857907732").send(`i just joined ${guild.name} im in  ${client.guilds.size} servers`);
   });
 };
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -3,6 +3,6 @@ module.exports = (client, guild) => {
     G.remove({ _id: guild.id }, (err) => {
         if(err) client.logger.error(err);
         else client.logger.log(`Deleted shit guild from db ${guild.name} (${guild.id})`);
-        client.channels.get("447177859857907732").send(`i just left ${guild.name} im in ${client.guilds.size} servers`);
+
     })
 }
