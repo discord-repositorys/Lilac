8efe78f5f6696956955fae69cf4865c728335088
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-24 16:15:43 -0400

    change reboot status

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -3,6 +3,5 @@ module.exports = (client, guild) => {
     G.remove({ _id: guild.id }, (err) => {
         if(err) client.logger.error(err);
         else client.logger.log(`Deleted shit guild from db ${guild.name} (${guild.id})`);
-
     })
 }
