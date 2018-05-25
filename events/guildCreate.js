fc954a04adb9f5dcdec3f1327f2cd90294c3e441
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-25 12:00:51 -0400

    fix prefix

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,6 +1,6 @@
 const G = require("../models/guild.js");
 module.exports = (client, guild) => {
-   G.create({ _id: guild.id, prefix: "d." }, (err, g) => {
+   G.create({ _id: guild.id, prefix: "l." }, (err, g) => {
   if(err) client.logger.error(err);
   else client.logger.log(`Added config for ${guild.name} (${guild.id})`);
   });
