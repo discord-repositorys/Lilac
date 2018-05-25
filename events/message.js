684e2ad78343ba28e7ca4a2e084d98390b2ea471
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-25 18:01:59 -0400

    check fix pls

--- events/message.js
+++ events/message.js
@@ -31,6 +31,10 @@ module.exports = async(client, message) => {
   }
   message.prefix = prefix;
 
+  if(message.content.match(new RegExp(`^<@!?${client.user.id}>$ prefix`))) {
+    message.channel.send("The prefix for this guild is ``${message.prefix}``")
+  }
+
   // Also good practice to ignore any message that does not start with our prefix,
   // which is set in the configuration file.
   if (message.content.indexOf(prefix) !== 0) return;
