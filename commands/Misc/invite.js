2c02549d5df8c0cf663300c5e0580b2ac4c9bfef
CyRIC21 <mcalts71@gmail.com>
2018-05-26 19:05:21 +0000

    edit invite

--- commands/Misc/invite.js
+++ commands/Misc/invite.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 exports.run = async (client, message, args, level) => {
-message.channel.send("https://discordapp.com/oauth2/authorize?client_id=435111581878059008&scope=bot&permissions=8");
+message.channel.send("https://discordapp.com/oauth2/authorize?client_id=449265397057716234&scope=bot&permissions=8");
 }
 exports.conf = {
     enabled: true,
