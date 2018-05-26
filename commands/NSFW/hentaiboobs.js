f4111707a0652096f3b30eca5eb37444d53794c2
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-26 12:51:13 -0400

    las

---
+++ commands/NSFW/hentaiboobs.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+const { MessageEmbed } = require("discord.js");
+
+module.exports.run = async(client, message, args, level) => {
+    
+    let {body} = await superagent
+    .get(`https://nekos.life/api/v2/img/boobs`);
+    if (!message.channel.nsfw) return message.reply("You must be in a N.S.F.W channel to use this command.");
+  
+    let hentaiEmbed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setTitle("Hentai")
+    .setImage(body.url)
+    .setFooter("Powered by nekos.life");
+
+
+    message.channel.send(hentaiEmbed);
+
+}
+
+exports.conf = {
+    aliases: [],
+    permLevel: "User"
+  };
+    
+  exports.help = {
+    name: 'hentaiboobs',
+    category: "NSFW",
+    description: 'Get a pic of hentai.',
+    usage: 'hentaiboobs'
+  };
