68b610c38d2b2eb6b000145c64f79f3ecb81b6e0
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-26 21:13:29 -0400

    test

--- commands/Server/userinfo.js
+++ commands/Server/userinfo.js
@@ -4,6 +4,7 @@ const { MessageEmbed } = require("discord.js");
 const UserInfo = new Discord.MessageEmbed()
 exports.run = function(client, message, args, level) {
 const UserInfo = new Discord.MessageEmbed()
+let target = message.author;
 .setAuthor(message.author.username, message.author.avatarURL()) //Heading With Username & Their Avatar 
 .setTitle('UserInfo')
 .setColor('RANDOM') //You Can Use HexColour Ex:- #000000
