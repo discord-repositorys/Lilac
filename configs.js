e0c8aeef834679db178f983fb1c8d38731a6f989
CyRIC <35691890+CyRIC21@users.noreply.github.com>
2018-05-28 19:51:02 -0400

    Delete configs.js

--- configs.js
+++
@@ -1,7 +0,0 @@
-{
-    TOKEN: "NDQ5MjY1Mzk3MDU3NzE2MjM0.DezAqA.HHHGr2VpfEH9n3KwDc7P8TGdbDA"
-    IDIOTICAPI: "dbTSZ1HS5cmU1oZaRdx7"
-    MONGODB: "mongodb://cryptide:bananaboigay@ds153869.mlab.com:53869/cryptide"
-    CLEVERBOT_USER: "p1OROwjS26diMwUW"
-    CLEVERBOT_APIKEY: "pJtsAa1IjsDhPrf4WjhJLoPUA8M05Fm6"
-}
